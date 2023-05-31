71585a4023a4a27ab218b5f5405afa75874fede5
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2023-05-31 14:38:20 +0300

    Update step1.sh

--- kubernetes-services/step1.sh
+++ kubernetes-services/step1.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-out=$(k get po -o json| jq -r '.items[]|[.metadata.labels.app, (.status.conditions[]|select(.type=="Ready")|.status)]|join(",")')
+out=$(kubectl get po -o json| jq -r '.items[]|[.metadata.labels.app, (.status.conditions[]|select(.type=="Ready")|.status)]|join(",")')
 if [ "$out" != "podinfo,True
 podinfo,True" ]; then
    echo pods not ready
    exit 1
 fi
 
-for ip in $(k get po -o json|jq -r '.items[]|.status.podIP'); do
+for ip in $(kubectl get po -o json|jq -r '.items[]|.status.podIP'); do
   if ! curl -fs $ip:9898/readyz >/dev/null ; then
     echo "pod failed to respond!"
     exit 1
