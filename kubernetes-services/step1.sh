28e0aab8d776a4e881022d9bf6f20a408d770e3e
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2023-05-31 14:44:55 +0300

    Update step1.sh

--- kubernetes-services/step1.sh
+++ kubernetes-services/step1.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+set -ex
+echo "checking pod readiness"
 out=$(kubectl get po -o json| jq -r '.items[]|[.metadata.labels.app, (.status.conditions[]|select(.type=="Ready")|.status)]|join(",")')
 if [ "$out" != "podinfo,True
 podinfo,True" ]; then
@@ -7,9 +8,16 @@ podinfo,True" ]; then
    exit 1
 fi
 
+echo "hitting pod with probe"
 for ip in $(kubectl get po -o json|jq -r '.items[]|.status.podIP'); do
   if ! curl -fs $ip:9898/readyz >/dev/null ; then
     echo "pod failed to respond!"
     exit 1
   fi 
-  done
+done
+
+
+
+echo "hitting svc with probe"
+ip=$(kubectl get svc -l nerd=someone -o go-template='{{ (index .items 0).spec.clusterIP }}')
+curl -fs $ip:9898
